afbdbec8642bd0fad94151eb0eab921ad640bb71
Juan Castillo <jach58@hotmail.com>
2018-12-30 20:52:36 -0600

    Primer commit rest-server

---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from './classes/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+
+const server = new Server();
+
+//Body Parser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+//cors
+server.app.use(cors({ origin: true, credentials: true }));
+
+//Rutas de servicios
+server.app.use('/', router);
+
+server.start(() => {
+    console.log(`Servidor corriendo en el servidor el puerto ${ server.port }`);
+});
