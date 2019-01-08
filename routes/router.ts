4895d2dcf92a0aa338422f16050a25b24fa41e7a
Juan Castillo <jach58@hotmail.com>
2019-01-07 22:12:32 -0600

    sección 6 lista

--- routes/router.ts
+++ routes/router.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import Server from '../classes/server';
+import { usuariosConectados } from '../sockets/socket';
 
 const router = Router();
 
@@ -49,6 +50,33 @@ router.post('/mensajes/:id', (req: Request, res: Response) => {
         de,
         id
     });
+});
+
+// servicios para obtener todos los IDs de los usuarios
+router.get('/usuarios', (req: Request, res: Response) => {
+    const server = Server.instance;
+
+    server.io.clients((err: any, clientes: string[]) => {
+        if(err) {
+            return res.json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            clientes
+        })
+    });
+});
+
+// obtener usuarios y sus nombres
+router.get('/usuarios/detalle', (req: Request, res: Response) => {
+    res.json({
+        ok: true,
+        clientes: usuariosConectados.getLista()
+    })
 });
 
 
